9a12f5dcf302db9d9c47060e9b84a2c2b3d1fedf
CauaTheKing <cauascp37@gmail.com>
2025-03-28 09:43:39 -0300

    PAGE & bun

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ServerTest from "./pages/ServerTest";
+// import ServerTest from "./pages/ServerTest";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +18,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/server-test" element={<ServerTest />} />
+
+          {/* <Route path="/server-test" element={<ServerTest />} /> */}
+
+
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,27 +1,28 @@
 
 import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+// import Header from '@/components/Header';
+
+// import Footer from '@/components/Footer';
 import { 
   HeroSection, 
   ExperienceSection, 
   SkillsSection, 
   EducationSection, 
-  ContactSection 
+  // ContactSection 
 } from '@/components/sections';
 
 const Index = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
-      <Header />
+      {/* <Header /> */}
       
       <HeroSection />
       <ExperienceSection />
       <SkillsSection />
       <EducationSection />
-      <ContactSection />
+      {/* <ContactSection /> */}
       
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
